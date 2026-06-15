37422bae65f3ab7a5e54f7231bb894c5e2ece9d2
tashidzen <tashidzen@yandex.ru>
2026-06-16 00:51:29 +0300

    fix: use official Node.js documentation for status codes and headers

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ const server = http.createServer((request, response) => {
   const url = new URL(request.url, `http://${hostname}:${port}`);
 
   if (url.searchParams.has("users")) {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+    response.setHeader = "Content-Type: application/json";
     response.write(getUsers());
     response.end();
 
@@ -18,31 +18,31 @@ const server = http.createServer((request, response) => {
     const name = url.searchParams.get("hello");
 
     if (name && name.trim() !== "") {
-      response.status= 200;
+      response.statusCode= 200;
       response.statusMessage = "OK";
-      response.header = "Content-Type: text/plain";
+      response.setHeader = "Content-Type: text/plain";
       response.write(`Hello, ${name}!`);
       response.end();
     } else {
-      response.status = 400;
+      response.statusCode = 400;
       response.statusMessage = "Bad Request";
-      response.header = "Content-Type: text/plain";
+      response.setHeader = "Content-Type: text/plain";
       response.write("Enter a name");
       response.end();
     }
     return;
   } else if (request.url === "/") {
-    response.status = 200;
+    response.statusCode = 200;
     response.statusMessage = "OK";
-    response.header = "Content-Type: text/plain";
+    response.setHeader = "Content-Type: text/plain";
     response.write("Hello, World!");
     response.end();
 
     return;
   } else {
-    response.status = 500;
+    response.statusCode = 500;
     response.statusMessage = "Internal Server Error";
-    response.header = "Content-Type: text/plain";
+    response.setHeader = "Content-Type: text/plain";
     response.write("");
     response.end();
   }
